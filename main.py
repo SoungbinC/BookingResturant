0149aea22a764b9833eb03a9043a55ae312ebc6d
Soungbin Cho <soungbinc@gmail.com>
2025-04-19 13:47:18 -0700

    added temporay middleware for integrating with frontend

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ from routers import (
     admin_router,
     profile_router,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(profile_router)
